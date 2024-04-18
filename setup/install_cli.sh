ffdee935b8f1d19241529dda0a35fa6924e2d2ad
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-04-18 09:25:05 -0400

    multi: install lncli from fork so that endorsement is available
    Otherwise not surfaced if we use the release binary.

--- setup/install_cli.sh
+++ setup/install_cli.sh
@@ -6,11 +6,9 @@ ARCH=$(dpkg --print-architecture)
 if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
     echo "Installing bitcoin / lnd for $ARCH"
     BITCOIN_URL="https://bitcoincore.org/bin/bitcoin-core-26.1/bitcoin-26.1-aarch64-linux-gnu.tar.gz"
-    LND_URL="https://github.com/lightningnetwork/lnd/releases/download/v0.17.4-beta/lnd-linux-arm64-v0.17.4-beta.tar.gz"
 elif [ "$ARCH" = "amd64" ]; then
     echo "Installing bitcoin / lnd for amd64"
     BITCOIN_URL="https://bitcoincore.org/bin/bitcoin-core-26.1/bitcoin-26.1-x86_64-linux-gnu.tar.gz"
-    LND_URL="https://github.com/lightningnetwork/lnd/releases/download/v0.17.4-beta/lnd-linux-amd64-v0.17.4-beta.tar.gz"
 else
     echo "Unsupported architecture $ARCH"
     exit 1
@@ -24,10 +22,13 @@ wget "$BITCOIN_URL"
 tar -xvf "$(basename "$BITCOIN_URL")"
 mv "$bitcoin_dir/bin/bitcoin-cli" /bin
 
-# Download and setup LND
-wget "$LND_URL"
-tar -xvf "$(basename "$LND_URL")"
-mv "$(basename "$LND_URL" .tar.gz)/lncli" /bin
+# Download LND and install it on custom branch so that endorsement signals are available on lncli.
+git clone https://github.com/carlaKC/lnd 
+cd lnd
+git checkout attackathon
+make install
+mv /root/go/bin/lncli /bin
+cd .. 
 
 # Clean up downloaded files: both the unpacked dirs and the targz.
-rm -rf "$bitcoin_dir" "$(basename "$BITCOIN_URL")" "$(basename "$LND_URL")"  "$(basename "$LND_URL" .tar.gz)"
+rm -rf "$bitcoin_dir" "$(basename "$BITCOIN_URL")" lnd
